2655f49ddddf52f93e4122ddd01cc0068d05aefd
brennop <brennopcordeiro@gmail.com>
2022-07-10 13:52:16 -0300

    🗣 head

--- pages/[file].tsx
+++ pages/[file].tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import { useRef } from "react";
 
 import Main from "../components/main";
@@ -9,7 +10,10 @@ import mdToHtml, { parseFrontmatter } from "../lib/mdToHtml";
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const files = getFiles();
 
-  const file = getFile(params?.file as string);
+  const filename = params?.file as string;
+
+  const file = getFile(filename);
+  const name = filename.replace(/\.md$/, "");
 
   const { content, data } = parseFrontmatter(file);
   const html = await mdToHtml(content);
@@ -17,11 +21,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       files: files,
+      name: name,
       content: html,
       data: data,
     },
   };
-}
+};
 
 export async function getStaticPaths() {
   const files = getMarkdownFiles();
@@ -41,29 +46,39 @@ export async function getStaticPaths() {
 type Props = {
   files: string[];
   content: string;
+  name: string;
   data: {
     title: string;
     date: string;
-  }
+  };
 };
 
-export default function File({ files, content, data }: Props) {
+export default function File({ files, content, data, name }: Props) {
   const constraintsRef = useRef(null);
 
   return (
-    <Main files={files} ref={constraintsRef}>
-      <Window
-        constraintsRef={constraintsRef}
-        className="w-screen max-w-2xl h-full sm:h-5/6">
-        <div className="p-4">
-          <h1 className="text-2xl font-bold">{data.title}</h1>
-          <p className="text-sm text-gray-700">{data.date}</p>
-        </div>
-        <section
-          className="prose prose-h1:text-xl prose-h2:text-lg p-6 prose-p:text-justify"
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-      </Window>
-    </Main>
+    <>
+      <Head>
+        <title>brennim - {name}</title>
+        <meta name="description" content="site do brennim" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Main files={files} ref={constraintsRef}>
+        <Window
+          constraintsRef={constraintsRef}
+          className="w-screen max-w-2xl h-full sm:h-5/6"
+        >
+          <div className="p-4">
+            <h1 className="text-2xl font-bold">{data.title}</h1>
+            <p className="text-sm text-gray-700">{data.date}</p>
+          </div>
+          <section
+            className="prose prose-h1:text-xl prose-h2:text-lg p-6 prose-p:text-justify"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        </Window>
+      </Main>
+    </>
   );
 }
